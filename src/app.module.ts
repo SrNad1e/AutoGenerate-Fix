8924db8f126fff61c043d4b8bf510554194da2dd
Jose Luis Rodriguez Peñaranda <soltec_cucuta@hotmail.com>
2022-01-03 16:29:05 -0500

    agregados entornos de desarrollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { InvoicesModule } from './invoices/invoices.module';
+import { ConfigModule } from '@nestjs/config';
 import { CouponsModule } from './coupons/coupons.module';
 import { CustomersModule } from './customers/customers.module';
 import { OrdersModule } from './orders/orders.module';
 import { ShopsModule } from './shops/shops.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
+import { enviroments } from './enviroments';
 @Module({
 	imports: [
+		ConfigModule.forRoot({
+			envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+			isGlobal: true,
+		}),
 		InvoicesModule,
 		CouponsModule,
 		CustomersModule,
@@ -20,7 +23,5 @@ import { ProductsModule } from './products/products.module';
 		UsersModule,
 		ProductsModule,
 	],
-	//	controllers: [AppController],
-	//	providers: [AppService],
 })
 export class AppModule {}
