c3a71ee2bd404cf4f5eda6fc74d37f8f1ffab231
Jose Luis Rodriguez Peñaranda <soltec_cucuta@hotmail.com>
2022-01-03 16:50:26 -0500

    se agregan validacion del esquema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 import { InvoicesModule } from './invoices/invoices.module';
-import { ConfigModule } from '@nestjs/config';
 import { CouponsModule } from './coupons/coupons.module';
 import { CustomersModule } from './customers/customers.module';
 import { OrdersModule } from './orders/orders.module';
@@ -9,11 +10,16 @@ import { ShopsModule } from './shops/shops.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { enviroments } from './enviroments';
+import config from './config';
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+			load: [config],
 			isGlobal: true,
+			validationSchema: Joi.object({
+				//variables entornos que se desean desplegar name_variable: Joi.number.required()
+			}),
 		}),
 		InvoicesModule,
 		CouponsModule,
