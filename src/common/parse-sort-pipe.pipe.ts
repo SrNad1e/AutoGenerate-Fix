7a37c46845f7e20566e4423e40d73418066a9a0b
Jose Luis Rodriguez Peñaranda <soltec_cucuta@hotmail.com>
2022-01-07 15:01:48 -0500

    Solucionado error de null

--- src/common/parse-sort-pipe.pipe.ts
+++ src/common/parse-sort-pipe.pipe.ts
@@ -3,12 +3,16 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class ParseSortPipePipe implements PipeTransform {
 	transform(values: Record<string, string>, metadata: ArgumentMetadata) {
-		const keys = Object.keys(values);
-		const newValues: Record<string, number> = {};
-		keys.forEach((key) => {
-			newValues[key] = parseInt(values[key]);
-		});
+		if (values) {
+			const keys = Object.keys(values);
+			const newValues: Record<string, number> = {};
+			keys.forEach((key) => {
+				newValues[key] = parseInt(values[key]);
+			});
 
-		return newValues;
+			return newValues;
+		} else {
+			return values;
+		}
 	}
 }
