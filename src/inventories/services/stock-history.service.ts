7bc6b7139669de3d5e0c9dc5268a11752080eb17
Darklegion <onlin1992@gmail.com>
2022-06-13 10:38:07 -0500

    SE organiza error al agregar unidades sin inventario

--- src/inventories/services/stock-history.service.ts
+++ src/inventories/services/stock-history.service.ts
@@ -44,96 +44,87 @@ export class StockHistoryService {
 		user: Partial<User>,
 		companyId: string,
 	) {
-		let item;
-		try {
-			if (details.length === 0) {
-				throw new BadRequestException(`No hay productos para continuar`);
-			}
+		if (details.length === 0) {
+			throw new BadRequestException(`No hay productos para continuar`);
+		}
 
-			const validateQuantity = details.find((item) => !(item.quantity > 0));
+		const validateQuantity = details.find((item) => !(item.quantity > 0));
 
-			if (validateQuantity) {
+		if (validateQuantity) {
+			throw new BadRequestException(
+				`No se puede agregar productos con cantidades en 0`,
+			);
+		}
+
+		let document;
+		switch (documentType) {
+			case DocumentTypeStockHistory.TRANSFER:
+				document = await this.stockTransferModel.findById(documentId).lean();
+				break;
+			case DocumentTypeStockHistory.INPUT:
+				document = await this.stockInputModel.findById(documentId).lean();
+				break;
+			case DocumentTypeStockHistory.ORDER:
+				document = await this.orderModel.findById(documentId).lean();
+				break;
+			case DocumentTypeStockHistory.ADJUSTMENT:
+				document = await this.stockAdjustmentModel.findById(documentId);
+				break;
+			case DocumentTypeStockHistory.RETURNORDER:
+				document = await this.returnOrderModel.findById(documentId);
+				break;
+			default:
 				throw new BadRequestException(
-					`No se puede agregar productos con cantidades en 0`,
+					`El tipo de documento ${documentType} es invalido`,
 				);
-			}
+		}
 
-			let document;
-			switch (documentType) {
-				case DocumentTypeStockHistory.TRANSFER:
-					document = await this.stockTransferModel.findById(documentId).lean();
-					break;
-				case DocumentTypeStockHistory.INPUT:
-					document = await this.stockInputModel.findById(documentId).lean();
-					break;
-				case DocumentTypeStockHistory.ORDER:
-					document = await this.orderModel.findById(documentId).lean();
-					break;
-				case DocumentTypeStockHistory.ADJUSTMENT:
-					document = await this.stockAdjustmentModel.findById(documentId);
-					break;
-				case DocumentTypeStockHistory.RETURNORDER:
-					document = await this.returnOrderModel.findById(documentId);
-					break;
-				default:
-					throw new BadRequestException(
-						`El tipo de documento ${documentType} es invalido`,
-					);
-			}
+		if (!document) {
+			throw new BadRequestException(`La documento no existe`);
+		}
 
-			if (!document) {
-				throw new BadRequestException(`La documento no existe`);
-			}
+		const warehouse = this.warehousesService.findById(warehouseId);
+		if (!warehouse) {
+			throw new BadRequestException(`La bodega no existe`);
+		}
 
-			const warehouse = this.warehousesService.findById(warehouseId);
-			if (!warehouse) {
-				throw new BadRequestException(`La bodega no existe`);
-			}
+		const products = details.map((detail) => detail.productId.toString());
 
-			const products = details.map((detail) => detail.productId.toString());
+		const { totalDocs } = await this.productsService.findAll(
+			{
+				ids: products,
+				status: StatusProduct.ACTIVE,
+			},
+			user,
+			companyId,
+		);
 
-			const { totalDocs } = await this.productsService.findAll(
-				{
-					ids: products,
-					status: StatusProduct.ACTIVE,
-				},
-				user,
-				companyId,
+		if (totalDocs !== products.length) {
+			throw new BadRequestException(
+				`Un producto no existe o se encuentra inactivo, revise la lista de productos`,
+			);
+		}
+		//agregar al inventario
+		for (let i = 0; i < details.length; i++) {
+			const { productId, quantity } = details[i];
+			const product = await this.productsService.addStock(
+				productId,
+				quantity,
+				warehouseId,
 			);
 
-			if (totalDocs !== products.length) {
-				throw new BadRequestException(
-					`Un producto no existe o se encuentra inactivo, revise la lista de productos`,
-				);
-			}
-			//agregar al inventario
-			for (let i = 0; i < details.length; i++) {
-				const { productId, quantity } = details[i];
-				const product = await this.productsService.addStock(
-					productId,
+			if (product) {
+				const newHistory = new this.stockHistoryModel({
+					warehouse: warehouseId,
+					currentStock: product?.stock[0]?.quantity,
 					quantity,
-					warehouseId,
-				);
-
-				if (product) {
-					const newHistory = new this.stockHistoryModel({
-						warehouse: warehouseId,
-						currentStock: product?.stock[0]?.quantity,
-						quantity,
-						company: new Types.ObjectId(companyId),
-						product: productId,
-						documentType,
-						documentNumber: document.number,
-					});
-					await newHistory.save();
-					item = i;
-				}
+					company: new Types.ObjectId(companyId),
+					product: productId,
+					documentType,
+					documentNumber: document.number,
+				});
+				await newHistory.save();
 			}
-		} catch (error) {
-			if (item) {
-				//TODO: pendiente planear el reversar
-			}
-			return error;
 		}
 	}
 
@@ -142,94 +133,83 @@ export class StockHistoryService {
 		user: Partial<User>,
 		companyId: string,
 	) {
-		let item;
+		if (details.length === 0) {
+			throw new BadRequestException(`No hay productos para continuar`);
+		}
 
-		try {
-			if (details.length === 0) {
-				throw new BadRequestException(`No hay productos para continuar`);
-			}
+		const validateQuantity = details.find((item) => !(item.quantity > 0));
+		if (validateQuantity) {
+			throw new BadRequestException(
+				`No se puede eliminar productos con cantidades en 0`,
+			);
+		}
 
-			const validateQuantity = details.find((item) => !(item.quantity > 0));
-			if (validateQuantity) {
+		let document;
+		switch (documentType) {
+			case DocumentTypeStockHistory.TRANSFER:
+				document = await this.stockTransferModel.findById(documentId).lean();
+				break;
+			case DocumentTypeStockHistory.OUTPUT:
+				document = await this.stockOutputModel.findById(documentId).lean();
+				break;
+			case DocumentTypeStockHistory.ADJUSTMENT:
+				document = await this.stockAdjustmentModel.findById(documentId);
+				break;
+			case DocumentTypeStockHistory.ORDER:
+				document = await this.orderModel.findById(documentId).lean();
+				break;
+			default:
 				throw new BadRequestException(
-					`No se puede eliminar productos con cantidades en 0`,
+					`El tipo de documento ${documentType} es invalido`,
 				);
-			}
+		}
+		if (!document) {
+			throw new BadRequestException(`La documento no existe`);
+		}
 
-			let document;
-			switch (documentType) {
-				case DocumentTypeStockHistory.TRANSFER:
-					document = await this.stockTransferModel.findById(documentId).lean();
-					break;
-				case DocumentTypeStockHistory.OUTPUT:
-					document = await this.stockOutputModel.findById(documentId).lean();
-					break;
-				case DocumentTypeStockHistory.ADJUSTMENT:
-					document = await this.stockAdjustmentModel.findById(documentId);
-					break;
-				case DocumentTypeStockHistory.ORDER:
-					document = await this.orderModel.findById(documentId).lean();
-					break;
-				default:
-					throw new BadRequestException(
-						`El tipo de documento ${documentType} es invalido`,
-					);
-			}
-			if (!document) {
-				throw new BadRequestException(`La documento no existe`);
-			}
+		const warehouse = this.warehousesService.findById(warehouseId);
+		if (!warehouse) {
+			throw new BadRequestException(`La bodega no existe`);
+		}
 
-			const warehouse = this.warehousesService.findById(warehouseId);
-			if (!warehouse) {
-				throw new BadRequestException(`La bodega no existe`);
-			}
+		const products = details.map((detail) => detail.productId.toString());
 
-			const products = details.map((detail) => detail.productId.toString());
+		const { totalDocs } = await this.productsService.findAll(
+			{
+				ids: products,
+				status: StatusProduct.ACTIVE,
+			},
+			user,
+			companyId,
+		);
 
-			const { totalDocs } = await this.productsService.findAll(
-				{
-					ids: products,
-					status: StatusProduct.ACTIVE,
-				},
-				user,
-				companyId,
+		if (totalDocs !== products.length) {
+			throw new BadRequestException(
+				`Un producto no existe o se encuentra inactivo, revise la lista de productos`,
+			);
+		}
+
+		for (let i = 0; i < details.length; i++) {
+			const { productId, quantity } = details[i];
+
+			const product = await this.productsService.deleteStock(
+				productId,
+				quantity,
+				warehouseId,
 			);
 
-			if (totalDocs !== products.length) {
-				throw new BadRequestException(
-					`Un producto no existe o se encuentra inactivo, revise la lista de productos`,
-				);
+			if (product) {
+				const newHistory = new this.stockHistoryModel({
+					warehouse: warehouseId,
+					currentStock: product?.stock[0]?.quantity,
+					quantity: -quantity,
+					company: new Types.ObjectId(companyId),
+					product: productId,
+					documentType,
+					documentNumber: document.number,
+				});
+				await newHistory.save();
 			}
-
-			for (let i = 0; i < details.length; i++) {
-				const { productId, quantity } = details[i];
-
-				const product = await this.productsService.deleteStock(
-					productId,
-					quantity,
-					warehouseId,
-				);
-
-				if (product) {
-					const newHistory = new this.stockHistoryModel({
-						warehouse: warehouseId,
-						currentStock: product?.stock[0]?.quantity,
-						quantity: -quantity,
-						company: new Types.ObjectId(companyId),
-						product: productId,
-						documentType,
-						documentNumber: document.number,
-					});
-					await newHistory.save();
-
-					item = i;
-				}
-			}
-		} catch (error) {
-			if (item) {
-				//TODO: pendiente planear el reversar
-			}
-			throw error;
 		}
 	}
 }
