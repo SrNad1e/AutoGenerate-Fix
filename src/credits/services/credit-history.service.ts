9cb957088d8e9b34df3c4c644121d305627606b9
Darklegion <onlin1992@gmail.com>
2022-06-14 12:39:03 -0500

    Proceso de implementacion de carteras

--- src/credits/services/credit-history.service.ts
+++ src/credits/services/credit-history.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { AggregatePaginateModel } from 'mongoose';
+import { Order } from 'src/sales/entities/order.entity';
+
 import { CreditHistory } from '../entities/credit-history.entity';
 import { CreditsService } from './credits.service';
 
@@ -9,28 +11,50 @@ export class CreditHistoryService {
 	constructor(
 		@InjectModel(CreditHistory.name)
 		private readonly creditHistoryModel: AggregatePaginateModel<CreditHistory>,
+		@InjectModel(Order.name)
+		private readonly orderModel: AggregatePaginateModel<Order>,
 		private readonly creditsService: CreditsService,
 	) {}
 
-	async addCreditHistory() {
-		//validar monto disponible
+	async addCreditHistory(orderId: string, amount: number) {
+		const order = await this.orderModel.findById(orderId);
+
+		if (!order) {
+			throw new NotFoundException('Pedido no encontrado');
+		}
+		//validar monto
 		//actualizar la cartera
 		//crear el registro
 	}
 
-	async deleteCreditHistory() {
+	async deleteCreditHistory(orderId: string, amount: number) {
+		const order = await this.orderModel.findById(orderId);
+
+		if (!order) {
+			throw new NotFoundException('Pedido no encontrado');
+		}
 		//validar monto para debitar
 		//actualiar la cartera
 		//crear el registro
 	}
 
-	async frozenCreditHistory() {
+	async frozenCreditHistory(orderId: string, amount: number) {
+		const order = await this.orderModel.findById(orderId);
+
+		if (!order) {
+			throw new NotFoundException('Pedido no encontrado');
+		}
 		//validar monto a congelar
 		//actualiar la cartera
 		//crear el registro
 	}
 
-	async thawedCreditHistory() {
+	async thawedCreditHistory(orderId: string, amount: number) {
+		const order = await this.orderModel.findById(orderId);
+
+		if (!order) {
+			throw new NotFoundException('Pedido no encontrado');
+		}
 		//validar monto a descongelar
 		//actualiar la cartera
 		//crear el registro
