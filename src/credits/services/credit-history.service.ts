952dfbdf61516161593b39bcbb78fc5b6c9edbbf
Darklegion <onlin1992@gmail.com>
2022-06-14 11:37:12 -0500

    Se genera la validación de crédito

--- src/credits/services/credit-history.service.ts
+++ src/credits/services/credit-history.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { AggregatePaginateModel } from 'mongoose';
+import { CreditHistory } from '../entities/credit-history.entity';
+import { CreditsService } from './credits.service';
 
 @Injectable()
-export class CreditHistoryService {}
+export class CreditHistoryService {
+	constructor(
+		@InjectModel(CreditHistory.name)
+		private readonly creditHistoryModel: AggregatePaginateModel<CreditHistory>,
+		private readonly creditsService: CreditsService,
+	) {}
+
+	async addCreditHistory() {
+		//validar monto disponible
+		//actualizar la cartera
+		//crear el registro
+	}
+
+	async deleteCreditHistory() {
+		//validar monto para debitar
+		//actualiar la cartera
+		//crear el registro
+	}
+
+	async frozenCreditHistory() {
+		//validar monto a congelar
+		//actualiar la cartera
+		//crear el registro
+	}
+
+	async thawedCreditHistory() {
+		//validar monto a descongelar
+		//actualiar la cartera
+		//crear el registro
+	}
+}
