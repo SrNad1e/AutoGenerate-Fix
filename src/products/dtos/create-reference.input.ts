b73e5dedaf0026f243adfefb7db24c93ffd34c93
Darklegion92 <onlin1992@gmail.com>
2022-10-26 06:55:02 -0500

    Se agrega el campo de atributos de la referencia como nulleable

--- src/products/dtos/create-reference.input.ts
+++ src/products/dtos/create-reference.input.ts
@@ -78,6 +78,7 @@ export class CreateReferenceInput {
 
 	@Field(() => [String], {
 		description: 'Atributos de la referencia',
+		nullable: true,
 	})
-	attribIds: string[];
+	attribIds?: string[];
 }
