5862f31a04ac734811c4574aae890196d012effe
Darklegion92 <onlin1992@gmail.com>
2022-11-11 14:48:23 -0500

    Se relaizna cambios para credit en POS

--- src/sales/services/orders.service.ts
+++ src/sales/services/orders.service.ts
@@ -668,6 +668,7 @@ export class OrdersService {
 						order as Order,
 						user,
 						companyId,
+						order.customer._id.toString(),
 					);
 
 					newPayments = newPayments.concat(paymentsProccess);
