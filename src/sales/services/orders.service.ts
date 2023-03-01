a292b8cf5d0f87cc694fd0476ca759df78700776
Darklegion92 <onlin1992@gmail.com>
2023-03-01 04:17:19 -0500

    Se elimina el populate para la shop

--- src/sales/services/orders.service.ts
+++ src/sales/services/orders.service.ts
@@ -83,7 +83,6 @@ export class OrdersService {
 	constructor(
 		@InjectModel(Order.name) private readonly orderModel: PaginateModel<Order>,
 		@InjectModel(ReturnOrder.name)
-		private readonly returnModel: PaginateModel<ReturnOrder>,
 		private readonly customersService: CustomersService,
 		private readonly shopsService: ShopsService,
 		private readonly productsService: ProductsService,
